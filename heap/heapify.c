4bc5ae8309b3810bb58e6341429ef1d6344d3254
Siddharth Singh <siddharthsingh@users.noreply.github.com>
2015-01-08 12:06:13 +0530

    Comments in heapify.c

--- heap/heapify.c
+++ heap/heapify.c
@@ -3,20 +3,26 @@
 
 void main(){
 
-//int heap[9] = {20,100,80,60,45,50,30,25,10};
+
 int heap[8] = {10 , 60 , 80 , 25 , 45 , 50 , 30 , 20 };
 //printf("%d \n",sizeof(heap));
+
+//size of array 
 int size = 8;
 
+//printing the heap before applying heapify
 printHeap(heap , size); 
 
-heapify(heap , 0 , size);
+//position of node where we want to apply heapify
+int pos = 0;
+
+heapify(heap , pos , size);
 
 printHeap(heap, size);
 }
 
 void printHeap(int a[] , int size){
-//integer array is passed as pointers so if we use size of on a we get the pointer size . which is 8 (in 64 bit mc)
+//integer array is passed as pointers so if we use sizeof on 'a' we get the sizeof the pointer . which is 8 (in 64 bit m/c)
 //int arraySize = sizeof(a);
 //printf("%d \n",sizeof(int));
 printf("\nHeap :  ");
@@ -26,19 +32,19 @@ for(int i = 0 ; i< size; i++){
 
 }
 
-void heapify(int * a , int pos , int arraySize){
+void heapify(int * a , int pos , int heapSize){
 
 int leftChild = 2*pos + 1;
 int rightChild = 2*pos + 2;
 int max = pos;
 //int arraySize = sizeof(a)/sizeof(int); cannot be used here as arrays are passed as pointer .
 
-if(a[leftChild] > a[pos] &&  leftChild < arraySize){
+if(a[leftChild] > a[pos] &&  leftChild < heapSize){
 max = leftChild;
 
 }
 
-if(a[rightChild] > a[max] && rightChild < arraySize){
+if(a[rightChild] > a[max] && rightChild < heapSize){
 
 	max = rightChild;
 }
@@ -50,9 +56,11 @@ if(max != pos){
 
 }
 
+//we only apply heapify to non leaf elements  and in a heap number of number of non leaf elements are from o - n/2 (n=size of heap)
+if(max <= heapSize/2 && max != pos ){
 
-if(max <= arraySize/2 && max != pos ){
-heapify(a, max , arraySize);
+heapify(a, max , heapSize);
+
 }
 
 }
